5e2ae0a33e25748ff1a128d4caa78537216c601f
Marek Petlicki <marek.petlicki@flightdataservices.com>
2018-11-16 13:56:01 +0000

    WIP: FlightDataAccessor v3 compatibility

--- analysis_engine/node.py
+++ analysis_engine/node.py
@@ -43,8 +43,7 @@ from analysis_engine.library import (
 from analysis_engine.recordtype import recordtype
 from analysis_engine.settings import NODE_CACHE_OFFSET_DP
 
-# FIXME: a better place for this class
-from hdfaccess.parameter import MappedArray
+from flightdataaccessor.datatypes.parameter import MappedArray, Parameter as FlightDataParameter
 
 from flightdatautilities.numpy_utils import slices_int
 
@@ -612,7 +611,7 @@ def can_operate(cls, available, actype=A('Aircraft Type')):
         logger.warning(*args, **kwargs)
 
 
-class DerivedParameterNode(Node):
+class DerivedParameterNode(Node, FlightDataParameter):
     """
     Base class for DerivedParameters which overide def derive() method.
 
@@ -632,6 +631,22 @@ class DerivedParameterNode(Node):
     def __init__(self, name='', array=np.ma.array([], dtype=float),
                  frequency=1.0, offset=0.0, data_type=None, lfl=False, *args, **kwargs):
 
+        kwargs['array'] = array
+
+        # XXX: refactor the `lfl` argument
+        if lfl:
+            # Where derived parameter is created to represent an LFL parameter, allow
+            # it to declare it came from the LFL.
+            kwargs['source'] = 'lfl'
+        else:
+            kwargs['source'] = 'derived'
+
+        FlightDataParameter.__init__(self, name=name, frequency=frequency, offset=offset, *args, **kwargs)
+        Node.__init__(self, name=name, frequency=frequency, offset=offset, *args, **kwargs)
+
+    def __init__old(self, name='', array=np.ma.array([], dtype=float), frequency=1.0, offset=0.0, data_type=None,
+                    lfl=False, *args, **kwargs):
+
         # Set the array on the derive parameter first. Some subclasses of this
         # class will handle appropriate type conversion of the provided array
         # in __setattr__:
@@ -643,10 +658,14 @@ class DerivedParameterNode(Node):
 
         if data_type:
             self.data_type = data_type
+
+        # XXX: refactor the `lfl` argument
         if lfl:
             # Where derived parameter is created to represent an LFL parameter, allow
             # it to declare it came from the LFL.
-            self.lfl = True
+            kwargs['source'] = 'lfl'
+        else:
+            kwargs['source'] = 'derived'
 
         super(DerivedParameterNode, self).__init__(
             name=name, frequency=frequency, offset=offset, *args, **kwargs)
@@ -856,8 +875,15 @@ class MultistateDerivedParameterNode(DerivedParameterNode):
     data_type = 'Derived Multistate'
     node_type_abbr = 'Multistate'
 
-    def __init__(self, name='', array=np.ma.array([], dtype=int), frequency=1.0,
+    def __init__(self, name='', array=None, frequency=1.0,
                  offset=0.0, data_type=None, values_mapping={}, *args, **kwargs):
+
+        if array is None:
+            array = MappedArray([], values_mapping=values_mapping)
+
+        super(MultistateDerivedParameterNode, self).__init__(
+            name, array, frequency, offset, data_type, *args,
+            **kwargs)
 
         #Q: if no values_mapping set to None?
         if values_mapping:
@@ -866,10 +892,6 @@ class MultistateDerivedParameterNode(DerivedParameterNode):
             self.values_mapping = {}
 
         self.state = {v: k for k, v in six.iteritems(self.values_mapping)}
-
-        super(MultistateDerivedParameterNode, self).__init__(
-            name, array, frequency, offset, data_type, *args,
-            **kwargs)
 
     def get_derived(self, *args, **kwargs):
         node = super(MultistateDerivedParameterNode, self).get_derived(*args,
