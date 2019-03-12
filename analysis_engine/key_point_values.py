2de2d36de9a485308290209426a77e39886c786c
Josef Abbasikadijani <42640416+abbas01j@users.noreply.github.com>
2019-03-12 10:05:18 +0000

    Comparison with None fix (#336)

--- analysis_engine/key_point_values.py
+++ analysis_engine/key_point_values.py
@@ -6052,7 +6052,7 @@ class AltitudeLastUnstableDuringApproachBeforeGoAround(KeyPointValueNode):
         apps = np.ma.clump_unmasked(stable.array)
         for app in apps[:-1]:
             index = index_of_last_stop(stable.array != 'Stable', app, min_dur=2)
-            if index > app.stop - 1:
+            if index is not None and index > app.stop - 1:
                 # approach ended unstable
                 # we were not stable so force altitude of 0 ft
                 self.create_kpv(app.stop - 0.5, 0)
