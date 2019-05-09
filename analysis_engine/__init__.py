8f3df77c2e1999322e94ae29c15e3ccf9b51f180
Marek Petlicki <marek.petlicki@flightdataservices.com>
2019-05-09 15:25:20 +0100

    Fix: dynamic version number from the package

--- analysis_engine/__init__.py
+++ analysis_engine/__init__.py
@@ -1,6 +1,6 @@
 import pkg_resources
 
-requirement = pkg_resources.Requirement.parse('AnalysisEngine')
+requirement = pkg_resources.Requirement.parse('FlightDataAnalyzer')
 distribution = pkg_resources.working_set.find(requirement)
 
 __version__ = distribution.version if distribution else 'N/A'
