bugfix/AE-2514/DJ/Radio_altimeter_overflow_masking - This change refines the mask following overflow correction. (#417)

analysis_engine/library.py
@@ -5466,7 +5466,9 @@ def overflow_correction_array(array):
 
     biggest_step_up = np.ma.max(steps)
     biggest_step_down = np.ma.min(steps)
+
     # Only fix things that need fixing
+    delta = None
     if biggest_step_up or biggest_step_down:
         # Compute and apply the correction
         biggest_step = max(abs(x) for x in (biggest_step_up, biggest_step_down) if x is not None)
@@ -5483,12 +5485,24 @@ def overflow_correction_array(array):
             delta = 1024.0 * np.rint((np.min(array[midpoint:]) + 20) / 1024)
         else:
             # Rare case of perfectly balanced changes; make no changes.
-            delta = None
+            pass
+    else:
+        # Stray piece of data. Let's make sure it is at least positive!
+        delta = 1024.0 * np.rint(np.min(array + 20) / 1024)
 
-        if delta:
-            array -= delta
+    if delta:
+        # Adjust the data by offsetting by this delta.
+        array -= delta
 
-    return np.ma.array(data=array, mask=keep_mask)
+    # Where there was a jump, the original data was masked, which we no longer
+    # need. The mask may have been assigned to the exact point of the change or
+    # possibly one sample earlier or later, or even two of these samples.
+    # Therefore the masking across steps is hidden for the step +/- 1 index.
+    hide_steps = np.zeros_like(array)
+    for index in np.flatnonzero(steps != 0):
+        hide_steps[index-1:index+2] = 1
+
+    return np.ma.array(data=array, mask=np.logical_and(keep_mask, hide_steps == 0))
 
 
 def peak_curvature(array, _slice=slice(None), curve_sense='Concave',

tests/library_test.py
@@ -5056,7 +5056,7 @@ class TestOverflowCorrection(unittest.TestCase):
         first_pass = overflow_correction(radioA.array, None, None)
         resA = overflow_correction(first_pass, alt_baro, fast)
         sects = np.ma.clump_unmasked(resA)
-        self.assertEqual(len(sects), 8)
+        self.assertEqual(len(sects), 5)
         self.assertGreater(resA.max(), 5000)
         self.assertEqual(resA.min(), -2)
 
@@ -5065,7 +5065,7 @@ class TestOverflowCorrection(unittest.TestCase):
         first_pass = overflow_correction(radioB.array, None, None)
         resB = overflow_correction(first_pass, alt_baro, fast)
         sects = np.ma.clump_unmasked(resB)
-        self.assertEqual(len(sects), 9)
+        self.assertEqual(len(sects), 5)
         self.assertGreater(resB.max(), 5000)
         self.assertEqual(resB.min(), -2)
 
@@ -5083,7 +5083,7 @@ class TestOverflowCorrection(unittest.TestCase):
         resA = overflow_correction(first_pass, alt_baro, fast)
         sects = np.ma.clump_unmasked(resA)
         # 1 section for climb, one for descent
-        self.assertEqual(len(sects), 4)
+        self.assertEqual(len(sects), 2)
         self.assertGreater(resA.max(), 7500)
         self.assertEqual(resA.min(), 0)
 
@@ -5093,7 +5093,7 @@ class TestOverflowCorrection(unittest.TestCase):
         resB = overflow_correction(first_pass, alt_baro, fast)
         sects = np.ma.clump_unmasked(resB)
         # 1 section for climb, one for descent
-        self.assertEqual(len(sects), 4)
+        self.assertEqual(len(sects), 2)
         self.assertGreater(resB.max(), 7500)
         self.assertEqual(resB.min(), 0)
 
@@ -8282,7 +8282,8 @@ class TestNearestRunway(unittest.TestCase):
     '''
     Tests for Nearest Runway
     '''
-    airports = yaml.load(open(os.path.join(test_data_path, 'airports.yaml'), 'rb'))
+    airports = yaml.load(open(os.path.join(test_data_path, 'airports.yaml'), 'rb'),
+                         Loader=yaml.FullLoader)
     _airports = airports['airports']
     _expected = {
         '001': {